9ada9164e275a7ec9e7f11ef62d6fd635200d964
Madhusudan Tripathy <57895240+MadhusudanTripathy@users.noreply.github.com>
2023-05-13 20:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ function strip (word) {
     let regex = new RegExp('^(a |the |an )', 'i')
     return word.replace(regex, '').trim()
 }
-touristSpots = touristSpots.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
+touristSpots.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
 document.querySelector('#bands').innerHTML = 
-    touristSpots.map(item => `<li>${item}</li>`).join
+    touristSpots.map(item => `<li>${item}</li>`).join('')
