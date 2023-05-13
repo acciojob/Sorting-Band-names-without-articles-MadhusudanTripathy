c6c409ccfaceef2656c6e8b43d9d03c27826088f
Madhusudan Tripathy <57895240+MadhusudanTripathy@users.noreply.github.com>
2023-05-13 20:22:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,4 +7,4 @@ function strip (word) {
 }
 touristSpots = touristSpots.sort((a, b) => (strip(a) > strip(b)) ? 1 : -1)
 document.querySelector('#bands').innerHTML = 
-    sortedBands.map(item => `<li>${item}</li>`).join
+    touristSpots.map(item => `<li>${item}</li>`).join
